330cbdc9c6df152e1387eec2e96ab37e8e23ee3d
zainkhan101345-ai <zainkhan101345@gmail.com>
2026-08-25 06:12:54 -0700

    sh cmds changed to bat for testing

--- AppInfra/HostedZone/main.tf
+++ AppInfra/HostedZone/main.tf
@@ -1,9 +1,7 @@
+data "aws_route53_zone" "hosted_zone" {
+  name         = "zaink.store"
+}
+
 output "hosted_zone_id" {
-    value = aws_route53_zone.hosted_zone.zone_id
+  value = data.aws_route53_zone.hosted_zone.zone_id
 }
-
-resource "aws_route53_zone" "hosted_zone" {
-    name = "zaink.store"
-}
-
-
